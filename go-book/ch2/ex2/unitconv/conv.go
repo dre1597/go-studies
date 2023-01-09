d163119c2e9bdd9a05f49e37ec8d93268ac90992
André Pereira <andrepereira97@yahoo.com>
2023-01-08 23:23:05 -0300

    :fire: remove exercises code from the go book

--- go-book/ch2/ex2/unitconv/conv.go
+++
@@ -1,9 +0,0 @@
-package unitconv
-
-func PaToAtm(pa Pascal) Atmospheric {
-	return Atmospheric(pa / 101300)
-}
-
-func AtmToPa(atm Atmospheric) Pascal {
-	return Pascal(atm * 101300)
-}
--- go-book/ch2/ex2/unitconv/units.go
+++
@@ -1,9 +0,0 @@
-package unitconv
-
-import "fmt"
-
-type Pascal float64
-type Atmospheric float64
-
-func (pa Pascal) String() string       { return fmt.Sprintf("%g Pa", pa) }
-func (atm Atmospheric) String() string { return fmt.Sprintf("%g atm", atm) }
